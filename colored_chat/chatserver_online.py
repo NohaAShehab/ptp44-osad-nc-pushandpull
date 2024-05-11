2e4bb073d78b00343ff756d08d4fcd77fe3ce72b
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-05-11 11:51:29 +0300

    display online users

---
+++ colored_chat/chatserver_online.py
@@ -0,0 +1,59 @@
+# display online users -->
+# coloring chat -->
+
+from simple_websocket_server import WebSocketServer, WebSocket
+import json
+
+
+def get_message_content(message):
+    message = json.loads(message)
+    return message
+
+
+def prepare_message(message_obj, clients):
+    online_users = list(map(lambda client:client.username, clients))
+    print(online_users)
+    message = {"online":online_users}
+    username = message_obj['name']
+    if 'login' in message_obj and message_obj['login']:
+        message['content'] = f'{username} has been connected'
+    elif 'body' in message_obj and message_obj['body']:
+        message['content'] =  f"{username}: {message_obj['body']}"
+    #
+    data = json.dumps(message)
+    return data
+
+
+class ChatServer(WebSocket):
+    clients = []
+
+    @classmethod
+    def send_message_to_all(cls, message):
+        for client in cls.clients:
+            client.send_message(message)
+
+    def handle(self):  # this message is fired when I receive any message
+        received_message = get_message_content(self.data)
+        if 'login' in received_message:
+            self.username = received_message['name']
+        newmessage = prepare_message(received_message, self.__class__.clients)
+        self.__class__.send_message_to_all(newmessage)
+
+    def connected(self):
+        print(f"New client connected-> {self}")
+        self.__class__.clients.append(self)
+
+    def handle_close(self):
+        print(self.address, 'closed')
+        message=  {"content": f'{self.username} has been disconnected'}
+        self.__class__.clients.remove(self)
+        online_users = list(map(lambda client: client.username, self.__class__.clients))
+        message['online']= online_users
+        self.__class__.send_message_to_all(json.dumps(message))
+
+
+
+if __name__ == '__main__':
+    server = WebSocketServer('', 8000, ChatServer)
+    print(server)
+    server.serve_forever()
---
+++ colored_chat/client.js
@@ -0,0 +1,64 @@
+
+console.log("welcome to chat app")
+apptitle = document.getElementById("apptitle");
+content_div = document.getElementById('content')
+send_button = document.getElementById('send_button')
+message_input = document.getElementById('message')
+var onlineusers = document.getElementById('onlineusers')
+var username = prompt("Please enter your username: ")
+apptitle.innerHTML = `Chat App ${username}`
+
+// console.log(message_input, send_button, content_div)
+console.log(username)
+
+/// use websockets to open connection --> with me and the remote server
+// connection use ws (web socket server )
+
+// once I opened the client --> connect to the server --> websocket
+mywebsocket = new WebSocket('ws://localhost:8000');// instance connect to the server
+
+
+// mywebsocket --> set of methods .// events -->
+
+// send my name to the server once the server opened
+mywebsocket.onopen = function () {
+    // send message to the server
+    data = {
+        name: username,
+        login : true
+    }
+    data = JSON.stringify(data)
+    this.send(data)
+
+}
+
+
+// what I will do when I receive message ?
+mywebsocket.onmessage = function (event) {
+    // console.log('message received', event.data )
+    message_content = JSON.parse(event.data)
+    console.log(message_content)
+    content_div.innerHTML += `<h3>${message_content.content}</h3>`
+
+    online_users = message_content.online
+    onlineusers.innerHTML = ''
+    online_users.forEach((user)=>{
+       onlineusers.innerHTML += `<li>${user}</li>`
+    });
+}
+
+// allow client to send message ??
+send_button.addEventListener('click', function (event) {
+    usermessage =message_input.value
+    data = {
+        name: username,
+        body:usermessage
+    }
+    mywebsocket.send(JSON.stringify(data))
+    message_input.value=''
+})
+
+
+mywebsocket.onerror= function (event) {
+    content_div.innerHTML += `<h2 style="color:red;">Error connecting to server</h2>`
+}
