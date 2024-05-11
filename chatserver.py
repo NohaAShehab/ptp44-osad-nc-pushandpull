1aefd2fcb18f9d367380e148321eebad23aeec36
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-05-11 10:15:36 +0300

    handling connected users

--- chatserver.py
+++ chatserver.py
@@ -1,19 +1,34 @@
-
 from simple_websocket_server import WebSocketServer, WebSocket
+import json
+
+
+def get_message_content(message):
+    message = json.loads(message)
+    return message
 
 
 class ChatServer(WebSocket):
-    def handle(self):
-        print("--> frame received")
-        pass
+    clients = []
 
+    def handle(self):  # this message is fired when I receive any message
+        # print(f"--> message received: {self.data}, {type(self.data)}")
+        # when I receive any message --> I need to read its connect
+        received_message = get_message_content(self.data)
+        print(received_message)
+        # send it to all users
+        for client in self.__class__.clients:
+            client.send_message(f"{received_message['name']} has been connected".capitalize())
 
     def connected(self):
-        print("WebSocket connected")
-
+        # this function will be called when client connected
+        print(f"New client connected-> {self}")
+        # add connected client to the clients
+        self.__class__.clients.append(self)
 
     def handle_close(self):
         print(self.address, 'closed')
+        # remove the client from the list
+        self.__class__.clients.remove(self)
 
 
 if __name__ == '__main__':
--- client.js
+++ client.js
@@ -4,8 +4,35 @@ console.log("welcome to chat app")
 content_div = document.getElementById('content')
 send_button = document.getElementById('send_button')
 message_input = document.getElementById('message')
+var username = prompt("Please enter your username: ")
 
-console.log(message_input, send_button, content_div)
+// console.log(message_input, send_button, content_div)
+console.log(username)
 
 /// use websockets to open connection --> with me and the remote server
 // connection use ws (web socket server )
+
+// once I opened the client --> connect to the server --> websocket
+mywebsocket = new WebSocket('ws://localhost:8000');// instance connect to the server
+
+
+// mywebsocket --> set of methods .// events -->
+
+// send my name to the server once the server opened
+mywebsocket.onopen = function () {
+    // send message to the server
+    data = {
+        name: username,
+        login : true
+    }
+    data = JSON.stringify(data)
+    this.send(data)
+
+}
+
+
+// what I will do when I receive message ?
+mywebsocket.onmessage = function (event) {
+    // console.log('message received', event.data )
+    content_div.innerHTML += `<h3>${event.data}</h3>`
+}
